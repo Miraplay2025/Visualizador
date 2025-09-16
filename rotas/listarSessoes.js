50603d891e84df51f3559255a019993a107f8a55
Miraplay2025 <massingueantoniocarlos38@gmail.com>
2025-09-16 13:52:21 +0200

    Update listarSessoes.js

--- rotas/listarSessoes.js
+++ rotas/listarSessoes.js
@@ -1,6 +1,8 @@
 const { acessarServidor } = require("../utils/puppeteer");
 
 module.exports = async (req, res) => {
+  console.log(`[${new Date().toISOString()}] 🔹 Listar sessões solicitadas`);
+
   try {
     const resposta = await acessarServidor("listar_sessoes.php");
     res.json(resposta);
